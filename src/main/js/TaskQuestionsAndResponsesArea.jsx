26c2fb4f787c5cda99fc7d66ea5bdc045d339f56
Toby Dragon <dr.toby.dragon@gmail.com>
2021-05-15 20:29:29 -0400

    tests and fixes for questionCount

--- src/main/js/TaskQuestionsAndResponsesArea.jsx
+++ src/main/js/TaskQuestionsAndResponsesArea.jsx
@@ -1,18 +1,23 @@
 import QuestionAndResponseArea from "./QuestionAndResponseArea"
 
 /**
- * @prop {function} prevQuestion: 
- * @prop {function} nextQuestion: 
+ * @prop {function} prevQuestion
+ * @prop {function} nextQuestion
  * @prop {function} handleAnswerChange: function called when a response is selected, takes new string response as a param 
  * @prop {list of QuestionModel objects} questionModels
  * @prop {list of strings} currentAnswers
  * @prop {int} currentQuestionIndex: the index of the question model to display   
  */
 export const TaskQuestionsAndResponsesArea = (props) => {
+    const prevDisabled = props.currentQuestionIndex <= 0;
+    const nextDisabled = props.currentQuestionIndex >= props.questionModels.length-1;
+
     return (
         <div>
             <p data-testid="questionCount">{props.currentQuestionIndex+1}/{props.questionModels.length}</p>
             <QuestionAndResponseArea currentAnswer={props.currentAnswers[props.currentQuestionIndex]} questionModel={props.questionModels[props.currentQuestionIndex]} />
+            <button onClick={props.prevQuestion} disabled={prevDisabled}>Previous Question</button>
+            <button onClick={props.nextQuestion} disabled={nextDisabled}>Next Question</button>
         </div>
     );
 };
--- src/test/js/TaskQuestionsAndResponsesArea.test.js
+++ src/test/js/TaskQuestionsAndResponsesArea.test.js
@@ -14,14 +14,12 @@ describe("TaskQuestionsAndResponsesArea", ()=> {
         expect(screen.queryByText(/structure/)).toEqual(null);
         expect(screen.queryByText(/zone/)).toEqual(null);
     });
-
     test("checkQuestionSwitch1", ()=>{
         render(<TaskQuestionsAndResponsesArea questionModels={sampleQuestionList} currentAnswers={sampleAnswersList} currentQuestionIndex={1} />);
         expect(screen.queryByText(/plane/)).toEqual(null);
         expect(screen.queryByText(/structure/)).not.toEqual(null);
         expect(screen.queryByText(/zone/)).toEqual(null);
     });
-
     test("checkQuestionSwitch2", ()=>{
         render(<TaskQuestionsAndResponsesArea questionModels={sampleQuestionList} currentAnswers={sampleAnswersList} currentQuestionIndex={2} />);
         expect(screen.queryByText(/plane/)).toEqual(null);
@@ -33,14 +31,28 @@ describe("TaskQuestionsAndResponsesArea", ()=> {
         render(<TaskQuestionsAndResponsesArea questionModels={sampleQuestionList} currentAnswers={sampleAnswersList} currentQuestionIndex={0} />);
         expect(screen.getByTestId("questionCount").textContent).toEqual("1/3");
     });
-
     test("questionCountTest1", ()=>{
         render(<TaskQuestionsAndResponsesArea questionModels={sampleQuestionList} currentAnswers={sampleAnswersList} currentQuestionIndex={1} />);
         expect(screen.getByTestId("questionCount").textContent).toEqual("2/3");
     });
-
     test("questionCountTest2", ()=>{
         render(<TaskQuestionsAndResponsesArea questionModels={sampleQuestionList} currentAnswers={sampleAnswersList} currentQuestionIndex={2} />);
         expect(screen.getByTestId("questionCount").textContent).toEqual("3/3");
+    });
+
+    test("prevAndNextButtons0", ()=>{
+        render(<TaskQuestionsAndResponsesArea questionModels={sampleQuestionList} currentAnswers={sampleAnswersList} currentQuestionIndex={0} />);
+        expect(screen.getByText(/Next/)).not.toHaveAttribute("disabled");
+        expect(screen.getByText(/Previous/)).toHaveAttribute("disabled");
+    });
+    test("prevAndNextButtons1", ()=>{
+        render(<TaskQuestionsAndResponsesArea questionModels={sampleQuestionList} currentAnswers={sampleAnswersList} currentQuestionIndex={1} />);
+        expect(screen.getByText(/Next/)).not.toHaveAttribute("disabled");
+        expect(screen.getByText(/Previous/)).not.toHaveAttribute("disabled");
+    });
+    test("prevAndNextButtons2", ()=>{
+        render(<TaskQuestionsAndResponsesArea questionModels={sampleQuestionList} currentAnswers={sampleAnswersList} currentQuestionIndex={2} />);
+        expect(screen.getByText(/Next/)).toHaveAttribute("disabled");
+        expect(screen.getByText(/Previous/)).not.toHaveAttribute("disabled");
     });
 });
