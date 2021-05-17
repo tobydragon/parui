849d01948ac999542a342c695886adef49fd7450
Toby Dragon <dr.toby.dragon@gmail.com>
2021-05-17 09:07:43 -0400

    fixed suffix convention

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { DropdownResponseAreaDemo } from './test/js/DropdownResponseArea.demo';
 import { ImageAreaDemo } from './test/js/ImageAreaDemo';
 import ImageTaskDemo from './test/js/ImageTask.demo';
 import { QuestionAndResponseAreaDemo } from './test/js/QuestionAndResponseArea.demo';
-import StudentViewDemo from './test/js/StudentViewDemo';
+import StudentViewDemo from './test/js/StudentView.demo';
 import { TaskQuestionsAndResponsesAreaDemo } from './test/js/TaskQuestionsAndResponsesArea.demo';
 
 function App() {
